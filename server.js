792f912bfd4c798b6cd79f9ae5d8d34999d5a0e1
AadhilPaul <aadhilpaul246@gmail.com>
2025-03-12 03:11:15 +0530

    ffifth commit

--- server.js
+++ server.js
@@ -5,8 +5,16 @@ const { getQuizQuestions, submitResponse, getFinalScore} = require("./sheets"); 
 const app = express();
 const PORT = 3000;
 
-app.use(cors({origin: "*"}));
+app.use(cors());
 app.use(express.json()); // Middleware for JSON parsing
+// 
+// Manually set CORS headers (backup method)
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
 const finalScores = {};
 
